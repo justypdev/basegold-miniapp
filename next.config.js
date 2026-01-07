b5dbb362c7545d2a299c782df6fe8c4a02cd6e61
justypdev <jjp123abc@gmail.com>
2026-01-06 23:35:41 -0500

    Update webpack config to include async-storage fallback

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['basegold.io'],
-  },
-  // Required for OnchainKit
   webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      '@react-native-async-storage/async-storage': false,
+    };
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
   },
